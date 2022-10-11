2ed2bb851aed66bc58b4d42b76c4c8ae7cf66014
Daniel Alejandro Ojeda <ojedadanielalejandro333@gmail.com>
2022-10-11 12:47:29 -0300

    Hay que mejorar el logo

--- src/components/Logo.svelte
+++ src/components/Logo.svelte
@@ -1,8 +1,14 @@
 <script>
-export let appName = ''
-
+  export let appName = ''
 </script>
+
 <div class="Logo">
   <h1>{appName}</h1>
 </div>
-<style></style>
+
+<style>
+  h1 {
+    margin: 1rem;
+    font-size: 2rem;
+  }
+</style>
