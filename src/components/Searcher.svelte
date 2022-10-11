2dd36ea27f161c55c83203f378a3f20ce05af1d9
Daniel Alejandro Ojeda <ojedadanielalejandro333@gmail.com>
2022-10-11 12:48:41 -0300

    Buscador, falta terminar

--- src/components/Searcher.svelte
+++ src/components/Searcher.svelte
@@ -1,3 +1,13 @@
 <script></script>
-<div class="Searcher"></div>
-<style></style>
+
+<div class="Searcher">
+  <input type="text" name="" id="" placeholder="Buscar" />
+</div>
+
+<style>
+  input{
+    width: 100%;
+    height: 20px;
+    border-radius: 5px;
+  }
+</style>
