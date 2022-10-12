7f9f0bb8bd376a973ee856db006402a6d65b2ebf
Daniel Alejandro Ojeda <ojedadanielalejandro333@gmail.com>
2022-10-12 10:49:42 -0300

    funcion desde el onclick en el component superior

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import Header from './components/Header.svelte'
   import Dashboard from './components/Dashboard.svelte'
+  import { v4 } from 'uuid'
 
   let notes = [
     {
@@ -12,11 +13,36 @@
   ]
   let copyNotes = [...notes]
   let count = notes.length
+  function generateColor() {
+    const color = [
+      '#FFF8EA',
+      '#CDFCF6',
+      '#C8DBBE',
+      '#DFD3C3',
+      '#FFD1D1',
+      '#AEBDCA',
+      '#AAC4FF',
+    ]
+    const index = Math.floor(Math.random() * color.length)
+    return color[index]
+  }
+  function handleNew() {
+    const color = generateColor()
+    const id = v4()
+    const note = {
+      id: id,
+      title: '',
+      color: color,
+      text: '',
+    }
+    notes = [note, ...notes]
+    copyNotes = [...notes]
+  }
 </script>
 
 <main>
   <Header />
-  <Dashboard {notes} />
+  <Dashboard notes={notes} on:click={handleNew} />
 </main>
 
 <style>
